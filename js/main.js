0aef0b09445fd5f2eba7027d185a7bec59f26c5f
Yoo Seungmin <dnjfht@naver.com>
2022-11-24 05:15:05 +0900

    aespa fansite no firevase

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar",
+    },
+  });
+
+  var swiper = new Swiper(".inSwiper1", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  var swiper = new Swiper(".inSwiper2", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  var swiper = new Swiper(".inSwiper3", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  var swiper = new Swiper(".inSwiper4", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  var swiper = new Swiper(".inSwiper5", {
+    effect: "cards",
+    grabCursor: true,
+  });
+});
